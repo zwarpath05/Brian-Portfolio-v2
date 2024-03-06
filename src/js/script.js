a0a00fa039d26f063edc7962f07c207bd099569a
Brian <zwarpath05@gmail.com>
2024-03-06 20:05:14 +0800

    Update Script, Style and Index

--- src/js/script.js
+++ src/js/script.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener('scroll', () => {
         Sticky_Header();
         highlightNavLinks();
+        reveal();
         
     });
 
@@ -95,3 +96,17 @@ document.addEventListener("DOMContentLoaded", function() {
         }
 
     }
+
+    const reveal = () => {
+        var reveals = document.querySelectorAll(".reveal");
+        for (var i = 0; i < reveals.length; i++) {
+          var windowHeight = window.innerHeight;
+          var elementTop = reveals[i].getBoundingClientRect().top;
+          var elementVisible = 150;
+          if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+          } else {
+            reveals[i].classList.remove("active");
+          }
+        }
+      }
