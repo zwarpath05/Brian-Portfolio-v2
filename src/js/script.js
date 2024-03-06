2c03ab17183a4eb3de90b1ee4dea9b67c20050be
Brian <zwarpath05@gmail.com>
2024-03-06 13:57:08 +0800

    update files bootstrap

---
+++ src/js/script.js
@@ -0,0 +1,97 @@
+const $ = (selector) => {
+    return document.querySelectorAll(selector).length > 1 ? document.querySelectorAll(selector) : document.querySelector(selector);
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    let navLink = $(".nav-link");
+    const sections = Array.from($('.section'));
+    let sticky = $(".hero").offsetTop;
+
+    window.addEventListener('scroll', () => {
+        Sticky_Header();
+        highlightNavLinks();
+        
+    });
+
+    const Sticky_Header = ()=> {
+        if (window.scrollY > sticky) {
+            $("#Back_To_Top").classList.add("ScrollTop");
+        }else {
+            $("#Back_To_Top").classList.remove("ScrollTop"); 
+        }
+    }
+
+    function highlightNavLinks() {
+        let current = '';
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (scrollY >= (sectionTop - sectionHeight / 3)) {
+                current = section.getAttribute('id');
+            }
+        });
+        navLink.forEach(navItem => {
+            navItem.classList.remove('active');
+            navItem.removeAttribute("aria-current");
+            if (navItem.getAttribute('href').slice(1) === current) {
+                navItem.classList.add('active');
+                navItem.setAttribute("aria-current", "true");
+            }
+        });
+    }
+
+});
+
+
+
+    // IMAGE GALLERY
+    let images = []
+    const imageElements = document.querySelectorAll(".Image_Gallery img");
+
+    imageElements.forEach(img => {
+        const srcValue = img.getAttribute("src");
+        images.push(srcValue);
+    });
+
+    let currentImageIndex = 0;
+
+    const nextImage = () => {
+        if (images.length > 0) {
+            currentImageIndex = (currentImageIndex + 1) % images.length;
+            showImage(images[currentImageIndex]);
+        }
+    }
+    
+    // Function to show the previous image
+    const previousImage = () => {
+        if (images.length > 0) {
+            currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+            showImage(images[currentImageIndex]);
+        }
+    }
+    
+
+   const showImage = (src) => {
+        const expandedImgElement = $("#expanded_img");
+        const imageElement = $("#image");
+        
+        if(expandedImgElement && imageElement ){
+            expandedImgElement.style.display = "block";
+            imageElement.src = src;
+            document.querySelector(".container-fluid").style.display = "none";
+            document.body.style.overflow = "hidden";
+        }
+      
+    }
+  
+    const closeImage = () => {
+        const expandedImgElement = $("#expanded_img");
+        if(expandedImgElement){
+            expandedImgElement.style.display = "none";
+            document.querySelector(".container-fluid").style.display = "block";
+            document.body.style.overflow = "auto";
+        }
+
+    }
