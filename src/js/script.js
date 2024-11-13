e6d0c96b0660c517015fee45a92fb8817e671a34
zwarpath05 <zwarpath05@gmail.com>
2024-11-13 11:28:35 +0800

    Index-v2 Updated

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,81 @@
 const $ = (selector) => {
-    return document.querySelectorAll(selector).length > 1
-      ? document.querySelectorAll(selector)
-      : document.querySelector(selector);
-  };
+  const elements = document.querySelectorAll(selector);
+  return elements.length === 1 ? [elements[0]] : elements;
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("load", checkMaxWidth);
+  window.addEventListener("resize", checkMaxWidth);
+  Slideshow_Carousel();
   
-  document.addEventListener("DOMContentLoaded", function () {
-    window.addEventListener("load", checkMaxWidth);
-    window.addEventListener("resize", checkMaxWidth);
+});
+
+const checkMaxWidth = () => {
+  const NavElement = $(".nav-bar");
+
+  if (window.innerWidth < 576) {
+    NavElement.classList.add("offcanvas");
+    NavElement.setAttribute("id", "offcanvasNavbar");
+  } else {
+    NavElement.classList.remove("offcanvas");
+    NaveElement.removeAttribute("id"); 
+  }
+};
+
+// SLIDESHOW CAROUSEL 
+const Slideshow_Carousel = () => {
+  let slideIndex = 0;
+  let time = 2000;
+  let interval;
+
+  const slider = $('.slideshow');
+  const slides = $('.slides');
+  const slidePreviewContainer  =  document.getElementById('slide-preview');
+
+  const  slidePreviewActive = ()  => {
+    slidePreviewContainer.innerHTML ='';
+    const ActiveSlide = slides[slideIndex].querySelector('img').cloneNode(true);
+    slidePreviewContainer.appendChild(ActiveSlide);
+    
+  }
+
+  const ShowSlide= () => {
+        slides[slideIndex].classList.remove('active');
+        slideIndex = (slideIndex + 1) % slides.length;
+        slides[slideIndex].classList.add('active');
+        slidePreviewActive();
+  }
+
+  
+
+  const startSlide = () => {
+    interval = setInterval(ShowSlide, time);
+  }
+
+  const stopSlide = () => {
+    clearInterval(interval);
+  };
+
+  slides[slideIndex].classList.add('active');
+  slidePreviewActive();
+  startSlide();
+
+  
+
+  slides.forEach((slide, arr_Index) => {
+    slide.addEventListener('mouseover', stopSlide);
+    slide.addEventListener('mouseout', startSlide);
+    slide.addEventListener('click', () => {
+      slides[slideIndex].classList.remove('active'); // Remove 'active' from current slide
+      slideIndex = arr_Index;                            // Set slideIndex to clicked slide's index
+      slides[slideIndex].classList.add('active');    // Add 'active' to the clicked slide
+      slidePreviewActive();
+      stopSlide();                                   // Stop the automatic sliding
+    });
   });
-  
-  const checkMaxWidth = () => {
-    const NavElement = $(".nav-bar");
-  
-    if (window.innerWidth < 576) {
-      NavElement.classList.add("offcanvas");
-      NavElement.setAttribute("id", "offcanvasNavbar");
-    } else {
-      NavElement.classList.remove("offcanvas");
-      NaveElement.removeAttribute("id");
-    }
-  };
-  
+
+}
+
+
+
+
